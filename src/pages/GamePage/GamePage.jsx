9cb2a052d51384192efeb0e4627dd85ccfd87f63
Jared Rairdon <jaredsrairdon@gmail.com>
2023-11-26 22:25:07 -0600

    added iframes for each game page based off hypen-title. adjusted html title

--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -1,13 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { supabase } from '../../apis/supabaseClient'
-
+import App from './../../../../brick-click/src/App'
+import './GamePage.css'
 
 function GamePage({ session }) {
   const { hyphenTitle } = useParams();
 
   return (
-    <p>{hyphenTitle}</p>
+    <>
+      <div className="gamepage-container">
+        {/* TODO: Pass Game object (or query db w/ hyphenTitle) to get
+                  game description and other metadata */}
+        <div className='gamepage-header'>
+          <h1>{hyphenTitle}</h1>
+        </div>
+        <div className="game-window">
+          <iframe
+            src={`https://jaredsrairdon.github.io/${hyphenTitle}/`}
+            title="Game Window"
+            id="game-iframe"
+            scrolling="yes"
+          />
+        </div>
+      </div>
+
+    </>
   )
 }
 
